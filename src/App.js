41a09dac6ebca01772e8cd26fdb38b6188994c78
Filipe Santana <filipe.santana@gmail.com>
2021-11-08 20:32:22 +0000

    Initial commit and initial implementation consuming api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
+import './card.style.scss';
 
 function App() {
+  const [ countries, setCountries ] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then( response => {
+        // handle success
+        setCountries(response.data)
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+  },[]);
+
+  console.log(countries);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Countries APP</h1>
+      <ul>
+        {countries.map((country) => {
+          return (
+            <div key={country.altSpellings}>
+              <div className="card-container">
+                <img className="card-flag" alt="flag" src={country.flags.png} />
+                <div className="card-description">
+                  <h3 className="card-title">{country.name.common}</h3>
+                  <p>{country.capital}</p>
+                  <p>{country.population}</p>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+        
+      </ul>
     </div>
   );
 }
